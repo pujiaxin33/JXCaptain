0f68d383fd2a599745232b9290d2dd9c9f364e97
pjx <pujx@jingbo360.com>
2019-08-20 16:53:33 +0800

    Initial Commit

---
+++ JXCaptain/JXCaptain.h
@@ -0,0 +1,19 @@
+//
+//  JXCaptain.h
+//  JXCaptain
+//
+//  Created by jiaxin on 2019/8/20.
+//  Copyright © 2019 jiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JXCaptain.
+FOUNDATION_EXPORT double JXCaptainVersionNumber;
+
+//! Project version string for JXCaptain.
+FOUNDATION_EXPORT const unsigned char JXCaptainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JXCaptain/PublicHeader.h>
+
+
