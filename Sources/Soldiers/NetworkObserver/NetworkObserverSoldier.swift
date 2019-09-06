1fae9cff2d38aa1c0bdff1c5f87c3efb3c54601a
pujiaxin33 <317437084@qq.com>
2019-09-06 21:12:10 +0800

    支持拦截WKWebView

--- Sources/Soldiers/NetworkObserver/NetworkObserverSoldier.swift
+++ Sources/Soldiers/NetworkObserver/NetworkObserverSoldier.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import WebKit
 
 private let kNetworkObserverSoldierIsActive = "kNetworkObserverSoldierIsActive"
 
@@ -23,6 +24,9 @@ public class NetworkObserverSoldier: Soldier {
             cache.countLimit = responseCacheByteLimit
         }
     }
+    /// 是否能拦截WKWebView的接口，默认为false。
+    /// 拦截的原理参考文章：https://blog.moecoder.com/2016/10/26/support-nsurlprotocol-in-wkwebview/ 因为WKWebView是一个单独的进程，如果要通过自定义的协议进行拦截，就会导致进程间通信，降低性能。所以，需要拦截WKWebView时，需要自己设置为true。
+    public var canInterceptWKWebView: Bool = false
     var isActive: Bool {
         set(new) {
             UserDefaults.standard.set(new, forKey: kNetworkObserverSoldierIsActive)
@@ -55,12 +59,18 @@ public class NetworkObserverSoldier: Soldier {
     }
 
     func start() {
+        if canInterceptWKWebView {
+            WKWebView.register(schemes: ["http", "https"])
+        }
         URLProtocol.registerClass(JXCaptainURLProtocol.self)
         URLSession.swizzleInit
         isActive = true
     }
 
     func end() {
+        if canInterceptWKWebView {
+            WKWebView.unregister(schemes: ["http", "https"])
+        }
         URLProtocol.unregisterClass(JXCaptainURLProtocol.self)
         isActive = false
     }
@@ -74,5 +84,56 @@ public class NetworkObserverSoldier: Soldier {
         DispatchQueue.main.async {
             NotificationCenter.default.post(name: NSNotification.Name.JXCaptainNetworkObserverSoldierNewFlowDidReceive, object: flowModel)
         }
+    }
+}
+
+extension String {
+    public var base64Decode: String? {
+        guard let data = Data(base64Encoded: self) else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+    public var base64Encode: String {
+        return Data(utf8).base64EncodedString()
+    }
+}
+
+extension WKWebView {
+    private class func browsing_contextController() -> (NSObject.Type)? {
+        guard let str = "YnJvd3NpbmdDb250ZXh0Q29udHJvbGxlcg==".base64Decode else { assertionFailure(); return nil }
+        // str: "browsingContextController"
+        guard let obj =  WKWebView().value(forKey: str) else { return nil }
+        return type(of: obj) as? NSObject.Type
+    }
+
+    private class func perform_browsing_contextController(aSelector: Selector, schemes: Set<String>) -> Bool {
+        guard let obj = browsing_contextController(), obj.responds(to: aSelector), schemes.count > 0 else {
+            assertionFailure(); return false
+        }
+        var result = schemes.count > 0
+        schemes.forEach({ (scheme) in
+            let ret = obj.perform(aSelector, with: scheme)
+            result = result && (ret != nil)
+        })
+        return result
+    }
+}
+
+extension WKWebView {
+    @discardableResult public class func register(schemes: Set<String>) -> Bool {
+        guard let str = "cmVnaXN0ZXJTY2hlbWVGb3JDdXN0b21Qcm90b2NvbDo=".base64Decode else {
+            assertionFailure(); return false
+        }
+        // str: "registerSchemeForCustomProtocol:"
+        let register = NSSelectorFromString(str)
+        return perform_browsing_contextController(aSelector: register, schemes: schemes)
+    }
+
+    @discardableResult public class func unregister(schemes: Set<String>) -> Bool {
+        guard let str = "dW5yZWdpc3RlclNjaGVtZUZvckN1c3RvbVByb3RvY29sOg==".base64Decode else {
+            assertionFailure(); return false
+        }
+        //str: "unregisterSchemeForCustomProtocol:"
+        let unregister = NSSelectorFromString(str)
+        return perform_browsing_contextController(aSelector: unregister, schemes: schemes)
     }
 }
